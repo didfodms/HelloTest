9659c2af6548e788a650e257bd1d3f8716a3c920
rae-eun <didfodms@gmail.com>
2022-04-06 20:03:13 +0900

    OOP homework 5

--- HelloTest/src/homework5_2.java
+++ HelloTest/src/homework5_2.java
@@ -1,50 +1,54 @@
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class homework5_1 {
+public class homework5_2 {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		SecureRandom random = new SecureRandom();
 		System.out.print("숫자 6개를 입력하세요 : ");
-		int[] input = new int[6];
-		int[] lotto = new int[6];
-		int[] correct = new int[6];
+		
+		ArrayList<Integer> input = new ArrayList<>();
+		ArrayList<Integer> lotto = new ArrayList<>();
+		ArrayList<Integer> correct = new ArrayList<>();
 		int count = 0;
 		
 		for(int i=0; i<6; i++) {
-			input[i] = sc.nextInt();
-			lotto[i] = random.nextInt(45) + 1;
-			//로또 번호 중복 처리
+			input.add(sc.nextInt());
+			lotto.add(random.nextInt(45)+1);
+			//로또 숫자 중복 처리
 			for(int j=0; j<i; j++) {
-				while(lotto[j]==lotto[i]) {
-					lotto[i] = random.nextInt(45) + 1;
+				while(lotto.get(i)==lotto.get(j)) {
+					lotto.set(i, random.nextInt(45)+1);
 				}
 			}
 		}
-		
+
 		for(int i=0; i<6; i++){
 			for(int j=0; j<6; j++) {
-				if(input[i]==lotto[j]) {
-					correct[count++] = input[i];
+				if(input.get(i)==lotto.get(j)) {
+					correct.add(input.get(i));
+					count++;
 				}
 			}
 		}
 		
 		System.out.print("사용자가 입력한 숫자 : ");
 		for(int i=0; i<6; i++) {
-			System.out.print(input[i]+" ");
+			System.out.print(input.get(i)+" ");
 		}
 		System.out.print("\n생성한 로또 숫자 : ");
 		for(int i=0; i<6; i++) {
-			System.out.print(lotto[i]+" ");
+			System.out.print(lotto.get(i)+" ");
 		}
 		System.out.print("\n일치하는 숫자의 개수 : "+count);
 		System.out.print("\n일치하는 숫자 : ");
 		for(int i=0; i<count; i++) {
-			System.out.print(correct[i]+" ");
+			System.out.print(correct.get(i)+" ");
 		}
+		
 	}
 
 }
