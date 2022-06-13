f84575a2ba65e366dbb4722a6ebae789ea5aec06
rae-eun <didfodms@gmail.com>
2022-06-13 16:23:06 +0900

    OOP problem1

---
+++ HelloTest/src/CollectionTest.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class CollectionTest {
+
+	private static void removeColors(Collection<String> collection1, Collection<String> collection2) {
+		
+		Iterator<String> iterator = collection1.iterator();
+		
+		while(iterator.hasNext()) {
+			if(collection2.contains(iterator.next())) {
+				iterator.remove();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		String[] colors = {"MEGENTA", "RED", "WHITE", "BLUE", "CYAN"};
+		List<String> list = new ArrayList<String>();
+		
+		for (String color : colors) {
+			list.add(color);
+		}
+		
+		String[] removeColors = {"RED", "WHITE", "BLUE"};
+		List<String> removeList = new ArrayList<String>();
+		
+		for(String color : removeColors) {
+			removeList.add(color);
+		}
+		
+		System.out.println("ArrayList: ");
+		
+		for(int count = 0; count < list.size(); count++) {
+			System.out.printf("%s ", list.get(count));
+		}
+		
+		removeColors(list, removeList);
+		
+		System.out.printf("%n%nArrayList after calling removeColors:%n");
+		
+		for(String color : list) {
+			System.out.printf("%s ", color);
+		}
+
+	}
+
+}
