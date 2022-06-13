7009d06ee821b2000dabac31938bc7865c053211
rae-eun <didfodms@gmail.com>
2022-06-13 17:53:48 +0900

    OOP problem solved

---
+++ HelloTest/src/GenericMethodTest.java
@@ -0,0 +1,26 @@
+
+public class GenericMethodTest {
+
+	public static void main(String[] args) {
+		
+		Integer[] integerArray = {1, 2, 3, 4, 5};
+		Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
+		Character[] characterArray = {'H', 'E', 'L', 'L', 'O'};
+		
+		System.out.printf("Array integerArray contains: ");
+		printArray(integerArray);
+		System.out.printf("Array doubleArray contains: ");
+		printArray(doubleArray);
+		System.out.printf("Array characterArray contains: ");
+		printArray(characterArray);
+	}
+	
+	public static <T> void printArray(T[] inputArray) {
+		for( T element : inputArray) {
+			System.out.printf("%s ", element);
+		}
+		
+		System.out.println();
+	}
+
+}
